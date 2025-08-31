d96052933371e6d085cf2024050556b26f53b5ac
HRUTHIK KV <sakvpal428@gmail.com>
2025-08-30 23:47:04 -0600

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,31 @@
+name="akash"
+age=20
+salary=100000000.8
+is_married=True
+print(f"{name},{age},{salary},{is_married}")
+print(type(age))
+print(type(is_married))
+print(type(name))
+age=float(age)
+print(age)
+salary=int(salary)
+print(salary)
+a=4
+b=2
+print(a+b)#addition
+print(a-b)#substraction
+print(a*b)#multiplication
+print(a/b)#division
+print(a//b)#floor division
+print(a%b)#it shows reminder
+print(a**b)#it is power
+a,b,c=10,20,25
+print(b)
+a=b=c=100
+print(c)
+a=2
+print(a)
+a=20
+b=30
+a,b=b,a
+print(b)
